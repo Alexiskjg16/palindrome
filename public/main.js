c5d20f664a5e0c86197f1a3a9699317214b1a41c
Alexis K Grisham <alexisgrisham@AlexisGMac.local>
2019-07-30 13:05:44 -0400

    forgot to commit

--- public/main.js
+++ public/main.js
@@ -1,5 +1,16 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+const updateWordEvent = () => {
+ const inputFieldValue = document.querySelector('.inputField').value
+ if (inputFieldValue ===inputFieldValue.split('').reverse().join(''))
+ return (document.querySelector('.yesOrNo').textContent = 'Yes')
+ else (document.querySelector('.yesOrNo').textContent = 'No')
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const addNewWordEvent = () => {
+  const previousInput = document.querySelector('.inputField').value
+  document.querySelector('.previousValue').textContent = previousInput
+  const previousAnswer = document.querySelector('.yesOrNo').value
+  document.querySelector('.previousAnswer').textContent = previousAnswer
+}
+
+document.querySelector('.trueOrFalse').addEventListener('click', updateWordEvent)
+document.querySelector('.trueOrFalse').addEventListener('click', addNewWordEvent)
