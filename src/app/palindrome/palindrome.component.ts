b5d106ed9ddb7fb81cca5586c8e13a2ead06f5a9
Alexis K Grisham <alexisgrisham@AlexisGMac.lan1>
2019-08-05 14:57:50 -0400

    updatePush

--- src/app/palindrome/palindrome.component.ts
+++ src/app/palindrome/palindrome.component.ts
@@ -1,29 +1,70 @@
-import { Component, OnInit } from '@angular/core';
-
-
-interface PalindromeObject {
- inputField: string;
- answerField: string;
-}
+import { Component, OnInit, ViewChild, QueryList, Input, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-palindrome',
   templateUrl: './palindrome.component.html',
   styleUrls: ['./palindrome.component.css']
-})
-export class PalindromeComponent implements OnInit {
+   })
 
-  constructor() { }
+export class PalindromeComponent {
+  @Input() inputName;
+   public showYes: string;
+   public showNo: string;
+ 
+    
+    constructor() {
+    }
+  ​
+    ngOnInit() {
+    }
+    
+      @ViewChild('inputName', {static: false})
+      inputValue: 'inputName';
 
-  ngOnInit() {
+    
+    isPalindrome() {
+       console.log(this.inputValue)
+    }
   }
 
-  updateWordEvent() {
-    const inputFieldValue = 'inputField'
-    if (inputFieldValue ===inputFieldValue.split('').reverse().join(''))
-    return document.getElementById("yesOrNo").innerHTML = `You got it right!` ;
-    else document.getElementById("yesOrNo").innerHTML = `You got it wrong!` ;
-   }
+//     isPalindrome() {
+//   if (this.inputValue.split('').reverse().join('') === this.inputValue) {
+//                   console.log(this.inputValue)
+//                 this.showYes = "You got it!"; }
+//                 else {
+//                 this.showNo = "Youre wrong";
+//               } 
+              
+//   }
+// 
 
-   
-}
+  
+  
+//   
+// export class PalindromeComponent implements OnInit {
+//   ​  public inputValue: string;
+//     public showYes: boolean;
+//     public showNo: boolean;
+ 
+    
+//     constructor() {
+//       this.inputValue = ''
+//     }
+//   ​
+//     ngOnInit() {
+//     }
+    
+
+//     isPalindrome(event: any) {
+
+//       ​this.inputValue = event.target.value
+//           if (this.inputValue.split('').reverse().join('') === this.inputValue) {
+//             console.log(this.inputValue.split(''))
+//           this.showYes = true;
+//           this.showNo = false;
+//         } else {
+//           this.showNo = true;
+//           this.showYes = false
+//         }
+//          }
+//       
