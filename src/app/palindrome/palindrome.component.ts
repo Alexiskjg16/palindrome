ee7a45314822136ccabfa5e908dc06dea68183e2
Alexis K Grisham <alexisgrisham@AlexisGMac.lan1>
2019-08-05 15:15:16 -0400

    test

--- src/app/palindrome/palindrome.component.ts
+++ src/app/palindrome/palindrome.component.ts
@@ -22,49 +22,19 @@ export class PalindromeComponent {
       inputValue: 'inputName';
 
     
+
+  
+
     isPalindrome() {
-       console.log(this.inputValue)
-    }
-  }
-
-//     isPalindrome() {
-//   if (this.inputValue.split('').reverse().join('') === this.inputValue) {
-//                   console.log(this.inputValue)
-//                 this.showYes = "You got it!"; }
-//                 else {
-//                 this.showNo = "Youre wrong";
-//               } 
+  if (this.inputValue.split('').reverse().join('') === this.inputValue) {
+                  console.log(this.inputValue)
+                this.showYes = "You got it!"; }
+                else {
+                this.showNo = "Youre wrong";
+              } 
               
-//   }
-// 
+  }
+}
 
   
-  
-//   
-// export class PalindromeComponent implements OnInit {
-//   ​  public inputValue: string;
-//     public showYes: boolean;
-//     public showNo: boolean;
  
-    
-//     constructor() {
-//       this.inputValue = ''
-//     }
-//   ​
-//     ngOnInit() {
-//     }
-    
-
-//     isPalindrome(event: any) {
-
-//       ​this.inputValue = event.target.value
-//           if (this.inputValue.split('').reverse().join('') === this.inputValue) {
-//             console.log(this.inputValue.split(''))
-//           this.showYes = true;
-//           this.showNo = false;
-//         } else {
-//           this.showNo = true;
-//           this.showYes = false
-//         }
-//          }
-//       
